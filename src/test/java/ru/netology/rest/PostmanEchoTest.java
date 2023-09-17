96815254e56fc1f111baf620a071a834ca85602d
Anastasia <90128050+SantAlice@users.noreply.github.com>
2023-09-18 00:54:51 +0300

    check fail if data is different

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -17,6 +17,6 @@ public class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("I like Pizza"));
+                .body("data", equalTo("I hate pizza"));
     }
 }
